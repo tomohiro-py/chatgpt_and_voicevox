6723601b225da5e3e6cb18eec71ef654f21b3904
suto-tomohiro <87257794+suto-tomohiro@users.noreply.github.com>
2023-06-07 22:03:38 +0900

    initial commit

---
+++ asyncio_my_module.py
@@ -0,0 +1,180 @@
+import asyncio
+import pyaudio
+import wave
+import io
+import config
+from time import sleep
+import aiohttp
+import openai
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import multiprocessing
+import queue
+import logging
+import random
+
+
+def setup_openai():
+    openai.api_key = config.openai_api_key
+
+
+async def achat(messages, response_queue):
+
+    res = openai.ChatCompletion.acreate(
+        model=config.openai_model_name,
+        max_tokens=config.openai_max_tokens,
+        temperature=0,
+        stream=True,
+        messages=messages
+        )
+    
+    # print("Chat stream Start")
+    words = []
+    chat_response = []
+
+    async for chunk in await res:
+        choices = chunk.choices[0]
+
+        if 'content' in choices.delta.keys():
+            content = choices.delta.content
+            words.append(content)
+            chat_response.append(content)
+            print(content, end="", flush=True)
+
+            if  '。' in content or '？' in content or '！' in content:
+                sentence = ''.join(words)
+                await response_queue.put(sentence)
+                # print('Sentence was putted')
+                words.clear()
+
+        elif choices.finish_reason == 'stop':
+            # print('Chat stream End')
+            print('', flush=True)
+            await response_queue.put('[DONE]')
+        
+    return ''.join(chat_response)
+
+        
+async def voicevox_text_to_query(response_queue, query_queue):
+    while True:
+        try:
+            item = await asyncio.wait_for(response_queue.get(), timeout=15)
+            if item == '[DONE]':
+                await query_queue.put('[DONE]')
+                break
+        except asyncio.TimeoutError:
+            # print('reponse_queue is empty')
+            break
+        except Exception as e:
+            print(e)
+            break
+
+        # print(f"Query Start")
+        params = {'text': item, 'speaker': config.voicevox_charactor_id}
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post('http://127.0.0.1:50021/audio_query', params=params) as res:
+                byte_str = await res.read()
+                query = byte_str.decode('utf-8')
+
+        await query_queue.put(query)
+        response_queue.task_done()
+        # print("Query End")
+
+
+async def voicevox_query_to_synthesis(query_queue, synthesis_queue, co_process_queue) -> bytes:
+    
+    while True:
+        try:
+            audio_query_response_json = await asyncio.wait_for(query_queue.get(), timeout=15)
+            if audio_query_response_json == '[DONE]':
+                co_process_queue.put('[DONE]')
+                break
+        except asyncio.TimeoutError:
+            # print('timeout:query_queue is empty')
+            break
+        except Exception as e:
+            print(e)
+            break
+        
+        # print(f"Synthesis Start")
+        params = {'speaker': config.voicevox_charactor_id}
+        headers = {'content-type': 'application/json'}
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                'http://127.0.0.1:50021/synthesis', 
+                data=audio_query_response_json, 
+                headers=headers, 
+                params=params) as res:
+                content = await res.content.read()
+
+        await synthesis_queue.put(content)
+        co_process_queue.put(content)
+        query_queue.task_done()
+        # print("Synthesis End")
+
+
+def play_wavbytes(co_process_queue):
+
+    p = pyaudio.PyAudio()
+    chunk = 1024
+
+    while True:
+        try:
+            wav_file = co_process_queue.get(timeout=15)
+            if wav_file == '[DONE]':
+                # print('All processes was done!')
+                break
+        except queue.Empty:
+            # print('co_process_queue is empty')
+            break
+        except Exception as e:
+            print(e)
+            break
+
+        # print('Player Start')
+        wr = wave.open(io.BytesIO(wav_file))
+        stream = p.open(
+            format=p.get_format_from_width(wr.getsampwidth()),
+            channels=wr.getnchannels(),
+            rate=wr.getframerate(),
+            output=True
+        )
+        data = wr.readframes(chunk)
+        while data:
+            stream.write(data)
+            data = wr.readframes(chunk)
+        else:
+            stream.close()
+            sleep(.3)
+            # print('Player End')
+
+    p.terminate()
+
+
+async def async_chatgpt_to_voicevox(messages):
+# asyncio.to_thread() is may be good for pyaudio.try it later.
+    setup_openai()
+    
+    loop = asyncio.get_event_loop()
+    response_queue = asyncio.Queue()
+    query_queue = asyncio.Queue()
+    synthesis_queue = asyncio.Queue()
+    co_process_queue = multiprocessing.Manager().Queue()
+
+    with ProcessPoolExecutor(max_workers=2) as executer:
+        task0 = loop.run_in_executor(executer,play_wavbytes,co_process_queue)
+
+        async with asyncio.TaskGroup() as tg:
+            task1 = tg.create_task(achat(messages, response_queue))
+            task2 = tg.create_task(voicevox_text_to_query(response_queue, query_queue)) 
+            task3 = tg.create_task(voicevox_query_to_synthesis(query_queue, synthesis_queue, co_process_queue))
+    
+    return task1.result()
+
+if __name__ == "__main__":
+    
+    messages = [{'role': 'system', 'content': 'あなたは優秀なAIアシスタントです。箇条書きの場合でも、必ず句読点を挿入してください。'}, {'role': 'user', 'content': 'こんにちは！'}]
+    result = asyncio.run(async_chatgpt_to_voicevox(messages),debug=False)
+    print(result)
+    
---
+++ main.py
@@ -0,0 +1,56 @@
+import my_module as st
+import openai
+import time
+import config
+from itertools import zip_longest
+import asyncio_my_module as ast
+import asyncio
+
+openai.api_key = config.openai_api_key
+
+# st.voicevox_text_to_speech('ハロー！')
+def main():
+    with open('system.prompt', 'r', encoding='utf-8') as f:
+        system_content = f.read()
+
+    system_message = dict(role='system', content=system_content)
+    user_messages = []
+    ai_messages = []
+
+    while True:
+        # user_content = input('YOU : ')
+        user_content = st.speech_to_text()
+        print(f'YOU : {user_content}')
+
+        if user_content.lower() == "bye now":
+            st.play_wavfile('wav/ending.wav')
+            break
+        elif user_content == "おしまい":
+            st.play_wavfile('wav/ending.wav')
+            break
+        else:
+            user_dict = dict(role='user',content=user_content)
+            user_messages.append(user_dict)
+
+        messages = []
+        messages.append(system_message)
+
+        if len(ai_messages) == 0:
+            messages.extend(user_messages)
+        else:
+            for (user_message, ai_message) in zip_longest(user_messages, ai_messages):
+                messages.append(user_message)
+                if ai_message is not None:
+                    messages.append(ai_message)
+        
+        ai_content = asyncio.run(ast.async_chatgpt_to_voicevox(messages))
+        ai_dict = dict(role='assistant',content=ai_content)
+        ai_messages.append(ai_dict)
+
+        if config.openai_prompt_threshold < len(user_messages):
+            del user_messages[0]
+            del ai_messages[0]
+
+
+if __name__ == '__main__':
+    main()
