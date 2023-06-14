1fb57840cbe229901f2a96cae2bad4a7e63cc477
suto-tomohiro <87257794+suto-tomohiro@users.noreply.github.com>
2023-06-14 12:55:25 +0900

    pvporcupineのデモに成功

---
+++ picovoice_demo.py
@@ -0,0 +1,44 @@
+import pvporcupine
+from pvrecorder import PvRecorder
+
+import config
+
+def demo():
+    # keywords = ['jarvis']
+    keyword_paths = ['picovoice_model/ぼーるっこ_ja_windows_v2_2_0.ppn', 'picovoice_model/ひなあられ_ja_windows_v2_2_0.ppn']
+    model_path = 'picovoice_model/porcupine_params_ja.pv'
+    device_index = -1
+    sensitivities = [0.5] * len(keyword_paths)
+
+    try:
+        porcupine = pvporcupine.create(
+            access_key=config.picovoice_api_key,
+            # keywords=keywords
+            keyword_paths=keyword_paths,
+            model_path=model_path,
+            sensitivities=sensitivities,
+            )
+    except Exception as e:
+        print(e)
+
+    recorder = PvRecorder(
+        frame_length=porcupine.frame_length,
+        device_index=device_index)
+    recorder.start()
+    print('Listening ... (press Ctrl+C to exit)')
+
+    try:
+        while True:
+            pcm = recorder.read()
+            result = porcupine.process(pcm)
+
+            if result >= 0:
+              print(result)
+    except KeyboardInterrupt:
+        print('Stopping ...')
+    finally:
+        recorder.delete()
+        porcupine.delete()
+
+if __name__ == '__main__':
+    demo()
